a236223f55dd12a3030e829a6ebe97543a585fe7
jadenong <onghyjaden@gmail.com>
2020-12-10 09:21:24 +0000

    update it la

---
+++ src/pages/resume/index.js
@@ -0,0 +1,38 @@
+import Navbar from "../components/Navbar";
+import Header from "../components/Masthead";
+import Footer from "../components/Footer";
+import Donations from "../components/Donations";
+
+/**
+ *  @description
+ *  This is the root (or index) page of our application.
+ *
+ *  @summary
+ *  From the folder structure (being the index page), you can access this page via ---> http://localhost:3000/
+ * 
+ *  @requires
+ *  Your server to be running ---> Run `npm run dev` in your terminal
+ */
+
+function Home() {
+  return (
+    <div>
+        <Header />
+      <Navbar />
+      <section>
+        <div className="container">
+          <h1>Welcome to Supplies</h1>
+          <p>
+            Help contribute to the community by donating any supplies that you
+            can spare to those who need them the most! (especially with what's
+            going on in the world today)
+          </p>
+        </div>
+      </section>
+      <Donations />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
